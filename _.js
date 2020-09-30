131f7cc2d1e2757be2a7caef0032868e38e24a7d
Yevhenii Kalashnyk <35871920+ykalashn@users.noreply.github.com>
2020-09-30 15:50:54 +0300

    Clamp method tested

--- _.js
+++ _.js
@@ -1,5 +1,10 @@
-
-
+const _ = {
+    // Method 1: Clamps number within the inclusive lower and upper bounds
+    clamp(num, lower, upper) {
+      const maxNum = Math.max(num, lower);
+      return Math.min(maxNum, upper);
+    }, 
+};
 
 
 
