e50ed7d7e8caa206f1311c1d829a717d97d8d7c2
Yevhenii Kalashnyk <35871920+ykalashn@users.noreply.github.com>
2020-10-02 11:07:30 +0300

    has method tested

--- _.js
+++ _.js
@@ -9,36 +9,47 @@ const _ = {
     // If end is not specified, it's set to start with start then set to 0.
     // If start is greater than end the params are swapped to support negative ranges.
     inRange(num, start, end) {
-        if (end === undefined) {
-          end = start;
-          start = 0;
-        };
-        if (start > end) {
-          const temp = start;
-          start = end;
-          end = temp;
-        };
-        return (num >= start && num < end) ? true : false;
+      if (end === undefined) {
+        end = start;
+        start = 0;
+      };
+      if (start > end) {
+        const temp = start;
+        start = end;
+        end = temp;
+      };
+      return (num >= start && num < end) ? true : false;
     },
-    // Splits string into an array of its words.
+
+    // Method 3: Splits string into an array of its words.
     words(str) {
-        const arr = str.split(' ');
-        return arr;
+      const arr = str.split(' ');
+      return arr;
     },
-    // Pads string on the left and right sides if it's shorter than length.
+
+    // Method 4: Pads string on the left and right sides if it's shorter than length.
     // Padding characters are truncated if they can't be evenly divided by length.
     pad(str, len) {
-        const strLen = str.length;
-        const space = ' ';
-        if (strLen > len) {
-          return str;
-        } else if ((len - strLen) % 2 === 0) {
-          str = space.repeat((len - strLen) / 2) + str + space.repeat((len - strLen) / 2);
-        } else {
-          str = space.repeat((len - 1 - strLen) / 2) + str + space.repeat((len + 1 - strLen) / 2);
-        }
+      const strLen = str.length;
+      const space = ' ';
+      if (strLen > len) {
         return str;
+      } else if ((len - strLen) % 2 === 0) {
+        str = space.repeat((len - strLen) / 2) + str + space.repeat((len - strLen) / 2);
+      } else {
+        str = space.repeat((len - 1 - strLen) / 2) + str + space.repeat((len + 1 - strLen) / 2);
       }
+      return str;
+    },
+
+    // Method 5: Checks if path is a direct property of object.
+    has(obj, key) {
+      if (obj[key] !== undefined) {
+        return true
+      } else {
+        return false
+      };
+    },
 };
 
 
